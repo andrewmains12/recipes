757cedea502d16349923f7fe35719e9fd7a06e34
Andrew Mains <andrewmains12@berkeley.edu>
2012-12-18 17:16:06 -0800

    First commit

---
+++ src/recipes/models/user.clj
@@ -0,0 +1,1 @@
+(ns recipes.models.user)
---
+++ src/recipes/views/main.clj
@@ -0,0 +1,8 @@
+(ns recipes.views.main
+  (:require [recipes.views.common :as common])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/" []
+         (common/layout
+           [:div#content]))
