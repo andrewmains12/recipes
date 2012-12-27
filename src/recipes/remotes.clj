4f648c3335dc29f7094fd9f1d040faa3066a930b
Andrew Mains <andrewmains12@berkeley.edu>
2012-12-26 23:34:32 -0800

    Basic remote for recipes

---
+++ src/recipes/remotes.clj
@@ -0,0 +1,16 @@
+(ns recipes.remotes
+  (:use [noir.fetch.remotes])
+  )
+
+(defremote recipe
+  "Return recipe with id"
+  ;;TODO: implement me
+  [id]
+  {:ingredients [{:name "Chocolate" :amount 500 :units "lbs"}
+                 {:name "Vanilla" :amount 2 :units "tbsp"}]
+   :instructions ["Do something"
+                  "Do another thing"
+                  "Profit!"]
+   })
+
+  
