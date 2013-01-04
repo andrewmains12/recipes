a7b56945aafa36faf429efed4598a28152b4169c
Andrew Mains <andrewmains12@berkeley.edu>
2013-01-03 17:23:27 -0800

    WIP: started recipe-list controller

--- src/recipes/client/controllers/recipes.cljs
+++ src/recipes/client/controllers/recipes.cljs
@@ -10,9 +10,9 @@
   (:use [recipes.client.views.recipes :only [render]])
   )
   
-  
-(def sm
-  (let [me (state/machine "recipes")]
+;;TODO: DRY state machine definitions up with a macro  
+(def recipe-box
+  (let [me (state/machine "recipe-box")]
     ;;States
     (defstate me :normal
       (in [recipe]
@@ -29,7 +29,45 @@
 
     (defevent me :loaded [recipe]
       (state/unset me :loading)
-      (state/set me :normal recipe))    
+      (state/set me :normal recipe)) 
     me))
 
 
+
+
+(def recipe-index
+  (let [me (state/machine "recipe-list")]
+    (defstate me :loading
+      (in []
+          (render me)))
+
+    (defstate me :unselected
+      (in [recipe-list]
+          (render me recipe-list)
+          ))
+
+    (defstate me :selected
+      (in [recipe-id]
+          (render me recipe-id)))
+
+
+    (defevent me :select [recipe-id]
+      (state/unset me :unselected)
+      (state/set me :selected recipe-id)
+      (state/trigger recipe-box
+                     :change-recipe recipe-id)
+      )
+
+
+    (defevent me :refresh-recipes [] ;;Possibly take category here
+      (state/unset me :selected)
+      (state/set me :loading)
+      (letrem [recipe-list (recipe-index)]
+              (state/trigger me :loaded recipe-list))
+      )
+
+    (defevent me :loaded [recipe-list]
+      (state/unset me :loading)
+      (state/set me :unselected recipe-list)
+      )
+    me))
