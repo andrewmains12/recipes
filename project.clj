03cf4613c790136728ab7e52107cc1a0833232dc
Andrew Mains <andrewmains12@berkeley.edu>
2013-01-14 16:58:27 -0800

    Added speclj

--- project.clj
+++ project.clj
@@ -3,24 +3,29 @@
   :url "http://exampl.com/FIXME"   
   :plugins [[lein-cljsbuild "0.2.9"]
             [lein-git-deps "0.0.1-SNAPSHOT"]
+            [speclj "2.5.0"]
             ]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir-cljs "0.3.7"]
                  [enlive "1.0.1"]
                  [noir "1.3.0"]                 
                  [gaka "0.3.0"]
-                 
+
+                 [speclj "2.5.0"]                 
                  ;;cljs
-                 [jayq "1.0.0"]
                  [fetch "0.1.0-alpha2" :exclusions [org.clojure/clojure]]
                  [waltz "0.1.0-alpha1"]
                  [crate "0.2.3"]
                  [enfocus "1.0.0-beta2"]
                  ]
+
   :git-dependencies [["https://github.com/clojure/clojurescript.git"
                       "88b36c177ebd1bb49dbd874a9d13652fd1de4027"
                       ]]
   :cljsbuild {:builds {:dev {}}}
+
+  :test-paths ["spec/"]
+                  
   :source-paths ["src" "static-hiccup"
                  ;;Latest cljs release
                  ".lein-git-deps/clojurescript/src/clj"
